d933169a22ede9c03483aebf17f1651e5243cc6a
Neko110923 <neko110923@163.com>
2026-04-19 00:33:23 +0800

    修改版本字符串

--- src/main/java/io/slidermc/starlight/config/InternalConfig.java
+++ src/main/java/io/slidermc/starlight/config/InternalConfig.java
@@ -1,5 +1,5 @@
 package io.slidermc.starlight.config;
 
 public class InternalConfig {
-    public static final String VERSION_STRING = "Starlight 26.1";
+    public static final String VERSION_STRING = "Starlight 1.21.11-26.1";
 }
